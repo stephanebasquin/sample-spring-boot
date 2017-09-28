71a5780edebebe6b4e47f6a0bdadb662d4dae0ea
Julien ALBERT <julien.albert@free.fr>
2017-09-28 12:07:01 +0200

    Ajout d'un simple Test sans appel au contexte Spring

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +12,11 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public  void myTest() {
+		assertTrue(true);
 	}
 
 }
