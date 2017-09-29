4363fe185ea8b32187fa5903c897cac66172499c
jalbert33 <jalbert33@users.noreply.github.com>
2017-09-29 10:59:03 +0200

    Ajout de code dupliqué dans les tests

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -91,6 +91,29 @@ public class DemoApplicationTests {
 		else {
 			assertTrue(false);
 		}
+		
+		// Test de code dupliqué
+		Integer i = 0;
+		i = 1;
+		System.out.println("i:"+i);
+		i = 2;
+		System.out.println("i:"+i);
+		i = 3;
+		System.out.println("i:"+i);
+		i = 4;
+		System.out.println("i:"+i);
+		i = 5;
+		System.out.println("i:"+i);
+		i = 6;
+		System.out.println("i:"+i);
+		i = 7;
+		System.out.println("i:"+i);
+		i = 8;
+		System.out.println("i:"+i);
+		i = 9;
+		System.out.println("i:"+i);
+		i = 10;
+		System.out.println("i:"+i);
 	}
 
 }
