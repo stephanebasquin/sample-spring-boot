b1df7a164da43bc3cfd91ac1714f83f8ea73b9ba
stephane <Vous@exemple.com>
2019-05-16 10:41:29 +0200

    ajput des import

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,6 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
