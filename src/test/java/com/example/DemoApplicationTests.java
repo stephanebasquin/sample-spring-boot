49909ab8ca14a58e6502de4f2eff882a63f16b64
stephane <Vous@exemple.com>
2019-05-16 10:37:20 +0200

    ajout de plusieurs tests

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,7 +15,35 @@ public class DemoApplicationTests {
 	}
 
 }
+
 @Test
 public void plante_tout_le_temps(){
 	assertTrue( false );
 }
+
+@Ignore
+@Test
+public void plante_tout_le_temps(){
+	assertTrue( false );
+}
+
+public Integer additionner(Integer a, Integer b){
+        Integer c;
+        c=a+b;
+        System.out.println("a = " + a + ", b = " + b);
+	System.out.println("La somme vaut " + c); 
+        return(c);
+    } 
+
+@Test
+public void additionner_integer()
+    {
+        if (additionner(2,4)==5 ) {
+        	assertTrue( true );
+	}
+	else {
+		assertTrue( false );
+		logger.fatal("This is fatal : La somme ne vaut pas 5" );
+	}
+}
+
